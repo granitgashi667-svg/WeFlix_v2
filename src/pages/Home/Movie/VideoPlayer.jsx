95f7fdb061f6fb49c66f14703338ab73a8c198ee
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2026-03-27 18:26:32 +0700

    switch server 2 and 3

--- src/pages/Home/Movie/VideoPlayer.jsx
+++ src/pages/Home/Movie/VideoPlayer.jsx
@@ -4,8 +4,8 @@ import { FaServer } from 'react-icons/fa';
 
 const SOURCES = [
   { name: 'Server 1 (VidSrc.cc)', url: (id) => `https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=true` },
-  { name: 'Server 2 (VidSrc RU)', url: (id) => `https://vidsrc-embed.ru/embed/movie/${id}?autoPlay=true` },
-  { name: 'Server 3 (VidLink)', url: (id) => `https://vidlink.pro/movie/${id}?primaryColor=c45454&secondaryColor=a2a2a2&iconColor=eefdec&poster=true&title=true&nextbutton=false&player=jw&autoplay=true` },
+  { name: 'Server 2 (VidLink)', url: (id) => `https://vidlink.pro/movie/${id}?primaryColor=c45454&secondaryColor=a2a2a2&iconColor=eefdec&poster=true&title=true&nextbutton=false&player=jw&autoplay=true` },
+  { name: 'Server 3 (VidSrc RU)', url: (id) => `https://vidsrc-embed.ru/embed/movie/${id}?autoPlay=true` },
   { name: 'Server 4 (Super)', url: (id) => `https://multiembed.mov/?video_id=${id}&tmdb=1&autoPlay=true` },
 ];
 
